4af8c31a96e656e5d50e831be353416c941383b3
rebop <me@reboplim.com>
2026-07-24 20:14:06 +0800

    feat(nvim): Integrate VimTeX with Okular

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -21,7 +21,10 @@ require("lazy").setup({
             "lervag/vimtex",
             lazy = false,
             init = function()
-                vim.g.vimtex_view_method = "sioyek"
+                vim.g.vimtex_view_general_viewer = 'okular'
+                vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
+                vim.g.vimtex_compiler_method = 'latexmk'
+                vim.g.vimtex_quickfix_mode = 0
             end
         },
         { import = "plugins" },
